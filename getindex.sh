05cf9dbfa68a92e4d9da76bbf1948264988ed2a0
Nick Moskovitz <nmosko@lowell.edu>
2026-05-20 20:51:55 -0700

    add shell script to download index files

---
+++ getindex.sh
@@ -0,0 +1,28 @@
+for ((i=0; i<48; i++)); do
+    I=$(printf %02i $i)
+    wget https://portal.nersc.gov/project/cosmo/temp/dstn/index-5200/LITE/index-5200-$I.fits
+done
+for ((i=0; i<48; i++)); do
+    I=$(printf %02i $i)
+    wget https://portal.nersc.gov/project/cosmo/temp/dstn/index-5200/LITE/index-5201-$I.fits
+done
+for ((i=0; i<48; i++)); do
+    I=$(printf %02i $i)
+    wget https://portal.nersc.gov/project/cosmo/temp/dstn/index-5200/LITE/index-5202-$I.fits
+done
+for ((i=0; i<48; i++)); do
+    I=$(printf %02i $i)
+    wget https://portal.nersc.gov/project/cosmo/temp/dstn/index-5200/LITE/index-5203-$I.fits
+done
+for ((i=0; i<48; i++)); do
+    I=$(printf %02i $i)
+    wget https://portal.nersc.gov/project/cosmo/temp/dstn/index-5200/LITE/index-5204-$I.fits
+done
+for ((i=0; i<48; i++)); do
+    I=$(printf %02i $i)
+    wget https://portal.nersc.gov/project/cosmo/temp/dstn/index-5200/LITE/index-5205-$I.fits
+done
+for ((i=0; i<48; i++)); do
+    I=$(printf %02i $i)
+    wget https://portal.nersc.gov/project/cosmo/temp/dstn/index-5200/LITE/index-5206-$I.fits
+done
